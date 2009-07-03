a03b9089d726fda781d43fba2d2420fa2983879a
Carl Meyer <carl@dirtcircle.com>
2009-07-03 02:23:09 -0400

    fixed bug causing TemplateError on any form errors

--- markupfield/widgets.py
+++ markupfield/widgets.py
@@ -3,8 +3,11 @@ from django.contrib.admin.widgets import AdminTextareaWidget
 
 class MarkupTextarea(forms.widgets.Textarea):
     def render(self, name, value, attrs=None):
-        if value is not None: 
-            value = value.raw
+        if value is not None:
+            try:
+                value = value.raw
+            except AttributeError:
+                pass
         return super(MarkupTextarea, self).render(name, value, attrs)
 
 class AdminMarkupTextareaWidget(MarkupTextarea, AdminTextareaWidget):
